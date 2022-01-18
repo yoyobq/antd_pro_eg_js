b6ab0491b68e0d080aac87eea53def75911552fa
Bu Qiang@SSTS <yoyobq@hotmail.com>
2022-01-18 20:29:44 +0800

    feat: 更新 HelloWorld 组件，演示 Button 组件的使用。

--- src/pages/HelloWorld/index.jsx
+++ src/pages/HelloWorld/index.jsx
@@ -1,5 +1,5 @@
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card } from 'antd';
+import { Button, Card } from 'antd';
 // import styles from './Welcome.less'; // 样式部分暂时不用
 
 const HelloWorld = () => {
@@ -7,6 +7,9 @@ const HelloWorld = () => {
     <PageContainer>
       <Card>
         <h1>Hello World</h1>
+        <Button type="primary" danger>
+          迎难而上，开始学习
+        </Button>
       </Card>
     </PageContainer>
   );
