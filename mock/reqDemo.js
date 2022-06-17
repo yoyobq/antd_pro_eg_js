72aab9f82ef834bb0592dc30728432d63a34af7b
Bu Qiang@SSTS <yoyobq@hotmail.com>
2022-06-17 16:08:43 +0800

    refactor: 更新 reqDemo 组件及相关 mock 文件，用于演示更复杂的请求及响应。

--- mock/reqDemo.js
+++ mock/reqDemo.js
@@ -1,10 +1,27 @@
+const waitTime = (time = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
+
 export default {
   // 响应来自 /api/reqDemo 的 post 方式的请求
-  'POST /api/reqDemo': (req, res) => {
+  'POST /api/reqDemo': async (req, res) => {
+    // 获取 request 中携带的发起 request 时间的数据，
+    // 也就是 req.body.reqTime 请注意这种解构赋值的写法
+    const { reqTime } = req.body;
+
+    await waitTime(2000);
+
     res.status(200).send({
       success: true,
       data: {
-        sayHi: 'hi',
+        // 除了 request 发起时间外
+        reqTime,
+        // response 的数据中还带上了 response 发送时的数据
+        resTime: Date.now(),
       },
     });
   },
--- src/pages/ReqDemo/index.js
+++ src/pages/ReqDemo/index.js
@@ -1,21 +1,34 @@
 import { reqDemo } from '@/services/ant-design-pro/reqDemo';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Button, Card, message } from 'antd';
+import { useState } from 'react';
 
 const ReqDemo = () => {
+  const [reqTime, setReqTime] = useState(0);
+  const [resTime, setResTime] = useState(0);
   // async / await 是语法糖，可以让异步代码变同步
   const handleClick = async () => {
     // 尝试执行 try 中的代码
     try {
       // 提出的请求附带数据为空对象，演示用
-      const data = {};
+      // const data = {};
+      // 把当前时间作为 data 的内容随着 request 一起发送
+      setReqTime(Date.now());
+      const data = {
+        reqTime,
+      };
       // reqDemo 是异步的，
       // await 关键字保证了需要等 reqDemo() 给出 return
       // 才会执行接下去的代码
-      const msg = await reqDemo({ ...data });
 
-      if (msg.success) {
-        message.success('success');
+      // 把当前时间作为 data 的内容随着 request 一起发送
+      // 等待后台响应，并把响应结果存入 res
+      const res = await reqDemo({ ...data });
+
+      // 如果收到成功信号
+      if (res.success) {
+        // 从响应数据中取出后台生成的响应时间并改变 state
+        setResTime(res.data.resTime);
       }
     } catch (error) {
       // 一旦 try 中的代码出错，则执行 catch 中的代码
@@ -30,6 +43,11 @@ const ReqDemo = () => {
         <Button type="primary" onClick={handleClick}>
           点我发送请求
         </Button>
+        <p />
+        {/* 用于在页面上显示请求、响应时间的代码 */}
+        {reqTime ? <div>请求发送时间戳: {reqTime}</div> : null}
+        {resTime ? <div>响应发送时间戳: {resTime}</div> : null}
+        {resTime ? <div>响应间隔: {resTime - reqTime} 毫秒</div> : null}
       </Card>
     </PageContainer>
   );
